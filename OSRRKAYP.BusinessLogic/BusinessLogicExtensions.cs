1abdfb19370cb099704d77b3f486cbb7539314b8
MINEDUCYT <MINEDUCYT@YAMICOMPU>
2026-05-06 03:39:51 -0600

    feat: agregar clase a la carpeta OSRRKAYP.BusineslogicExtensions Realizado por :Oscar Andres Ramirez Romero

---
+++ OSRRKAYP.BusinessLogic/BusinessLogicExtensions.cs
@@ -0,0 +1,26 @@
+﻿using OSRRKAYP.DataAccess;
+using Mapster;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace OSRRKAYP.BusinessLogic
+{
+    public static class BusinessLogicExtensions
+    {
+        public static IServiceCollection AddBusinessLogicServices(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddMediatR(
+                cfg =>
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly())
+            );
+            services.AddDataAccessServices(configuration);
+
+            TypeAdapterConfig.GlobalSettings.Scan(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
