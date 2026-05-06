13ec1c1a1e6ed7279edf3cda9a08136962f53d20
MINEDUCYT <MINEDUCYT@YAMICOMPU>
2026-05-06 03:16:23 -0600

    feat: agregar clase a la carpeta OSRRKAYP.DataAccess  Realizado por : Karla Yamileth Ceren Pérez

---
+++ OSRRKAYP.DataAccess/DataAccessExtensions.cs
@@ -0,0 +1,26 @@
+﻿using OSRRKAYP.DataAccess.Interfaces;
+using OSRRKAYP.DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace OSRRKAYP.DataAccess
+{
+    public static class DataAccessExtensions
+    {
+        public static IServiceCollection AddDataAccessServices(
+            this IServiceCollection services,
+            IConfiguration configuration
+        )
+        {
+            services.AddDbContext<QuotationContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DbConnection") ??
+                    throw new InvalidOperationException("connection string 'DbContext not found '"))
+            );
+            services.AddTransient(typeof(IEfRepository<>), typeof(EfRepository<>));
+
+            return services;
+        }
+    }
+}
