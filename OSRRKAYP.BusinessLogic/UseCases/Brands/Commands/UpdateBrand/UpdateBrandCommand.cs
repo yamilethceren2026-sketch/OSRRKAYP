7cb4c9b36d6e4ef2ff851c71563be6068c44f8e7
MINEDUCYT <MINEDUCYT@YAMICOMPU>
2026-05-07 11:32:11 -0600

    Feat: agregar carpeta UpdateBrand y clases UpdateBrandCommand, UpdateBrandHandler en la caperta UpdateBrand Realizao por: Karla Yamileth Ceren Perez

---
+++ OSRRKAYP.BusinessLogic/UseCases/Brands/Commands/UpdateBrand/UpdateBrandCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using OSRRKAYP.BusinessLogic.DTOs;
+
+namespace OSRRKAYP.BusinessLogic.UseCases.Manufacturers.Commands.UpdateManufacturer;
+
+public record UpdateManufacturerCommand(UpdateManufacturerDto Request) : IRequest<int>;
---
+++ OSRRKAYP.BusinessLogic/UseCases/Brands/Commands/UpdateBrand/UpdateBrandHandler.cs
@@ -0,0 +1,30 @@
+﻿using OSRRKAYP.DataAccess.Interfaces;
+using OSRRKAYP.Entities;
+using Mapster;
+using MediatR;
+
+namespace OSRRKAYP.BusinessLogic.UseCases.Manufacturers.Commands.UpdateManufacturer;
+
+internal sealed class UpdateManufacturerHandler(IEfRepository<Manufacturer> _repository)
+    : IRequestHandler<UpdateManufacturerCommand, int>
+{
+    public async Task<int> Handle(UpdateManufacturerCommand command, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var existingManufacturer = await _repository.GetByIdAsync(command.Request.Id);
+
+            if (existingManufacturer is null) return 0;
+
+            existingManufacturer = command.Request.Adapt(existingManufacturer);
+
+            await _repository.UpdateAsync(existingManufacturer, cancellationToken);
+
+            return existingManufacturer.Id;
+        }
+        catch (Exception)
+        {
+            return 0;
+        }
+    }
+}
