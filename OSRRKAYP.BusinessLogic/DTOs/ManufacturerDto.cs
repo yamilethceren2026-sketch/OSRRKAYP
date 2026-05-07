08de8e79d6ce72f749cb8782279cfd47d5ca21c4
MINEDUCYT <MINEDUCYT@YAMICOMPU>
2026-05-06 19:12:59 -0600

    feat : agregar clase ManufacturerDto.cs a la carpeta DTOs   Realiado por : Oscar Andres Ramirez Romero

---
+++ OSRRKAYP.BusinessLogic/DTOs/ManufacturerDto.cs
@@ -0,0 +1,21 @@
+﻿namespace OSRRKAYP.BusinessLogic.DTOs
+{
+    public class ManufacturerDto
+    {
+        public string BrandName { get; set; } = string.Empty;
+    }
+
+    public class UpdateManufacturerDto
+    {
+        public int Id { get; set; }
+
+        public string BrandName { get; set; } = string.Empty;
+    }
+
+    public class ManufacturerDtoResponse
+    {
+        public int BrandId { get; set; }
+
+        public string BrandName { get; set; } = string.Empty;
+    }
+}
