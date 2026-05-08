520df7da25616faf847aed73953616bf8bd20a21
MINEDUCYT <MINEDUCYT@YAMICOMPU>
2026-05-08 10:06:09 -0600

    Feat: agregar la clase ProductDto en la carpeta DTOs Realizado por: Karla Yamileth Ceren Perez

---
+++ OSRRKAYP.BusinessLogic/DTOs/ProductDto.cs
@@ -0,0 +1,26 @@
+﻿namespace OSRRKAYP.BusinessLogic.DTOs;
+
+public class ProductDto
+{
+    public long ProductId { get; set; }
+
+    public int? BrandId { get; set; }
+
+    public string? SupplierName { get; set; }
+
+    public string? ProductCode { get; set; }
+
+    public string ProductName { get; set; } = string.Empty;
+
+    public string? ProductDescription { get; set; }
+
+    public string? ProductImage { get; set; }
+
+    public decimal? PriceUnitPurchase { get; set; }
+
+    public decimal? PriceUnitSale { get; set; }
+
+    public int? Stock { get; set; }
+
+    public bool ProductStatus { get; set; }
+}
