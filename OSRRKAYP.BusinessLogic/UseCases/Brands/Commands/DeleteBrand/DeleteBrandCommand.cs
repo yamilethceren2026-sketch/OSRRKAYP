98db09fb5e67f0f1a747a1d1823b76e8fd620ab0
MINEDUCYT <MINEDUCYT@YAMICOMPU>
2026-05-07 22:18:53 -0600

    Feat: agregar carpeta DeleteBrand y agregar las clases DeleteBrandCommand, DeleteBrandHandler Realizado por: Karla Yamileth Ceren Perez

---
+++ OSRRKAYP.BusinessLogic/UseCases/Brands/Commands/DeleteBrand/DeleteBrandCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace OSRRKAYP.BusinessLogic.UseCases.Manufacturers.Commands.DeleteManufacturer;
+
+public record DeleteManufacturerCommand(int Id) : IRequest<int>;
---
+++ OSRRKAYP.BusinessLogic/UseCases/Brands/Commands/DeleteBrand/DeleteBrandHandler.cs
@@ -0,0 +1,20 @@
+﻿using OSRRKAYP.DataAccess.Interfaces;
+using OSRRKAYP.Entities;
+using MediatR;
+
+namespace OSRRKAYP.BusinessLogic.UseCases.Manufacturers.Commands.DeleteManufacturer;
+
+internal sealed class DeleteManufacturerHandler(IEfRepository<Manufacturer> _repository)
+    : IRequestHandler<DeleteManufacturerCommand, int>
+{
+    public async Task<int> Handle(DeleteManufacturerCommand command, CancellationToken cancellationToken)
+    {
+        var existingManufacturer = await _repository.GetByIdAsync(command.Id);
+
+        if (existingManufacturer is null) return 0;
+
+        await _repository.DeleteAsync(existingManufacturer, cancellationToken);
+
+        return existingManufacturer.Id;
+    }
+}
