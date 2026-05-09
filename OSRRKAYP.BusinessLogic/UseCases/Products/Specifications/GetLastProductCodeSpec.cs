c067a984c5f9bc843db7ac76b20cf3aa089ccb6b
MINEDUCYT <MINEDUCYT@YAMICOMPU>
2026-05-09 00:25:27 -0600

    Feat: agregar la carpeta Specifications y la clase GetLastProductCodeSpec Realizado por: Karla Yamileth Ceren Perez

---
+++ OSRRKAYP.BusinessLogic/UseCases/Products/Specifications/GetLastProductCodeSpec.cs
@@ -0,0 +1,15 @@
+﻿using Ardalis.Specification;
+using OSRRKAYP.Entities;
+
+namespace OSRRKAYP.BusinessLogic.UseCases.Products.Specifications
+{
+    internal sealed class GetLastProductCodeSpec : Specification<Product>
+    {
+        public GetLastProductCodeSpec()
+        {
+            Query
+                .OrderByDescending(p => p.Id)
+                .Take(1);
+        }
+    }
+}
