4c9dae6203bc6b82b071acd49a6b9cfd4148f61d
Oscar Andres Ramirez Romero <barleyrom0@gmail.com>
2026-05-10 17:32:21 -0600

    Feat: agregar  carpeta Mappin y la clase MappingRegister.cs   Realizado por : Oscar Andres Ramirez Romero

---
+++ OSRRKAYP.BusinessLogic/Mappings/MappingRegister.cs
@@ -0,0 +1,18 @@
+﻿using OSRRKAYP.BusinessLogic.DTOs;
+using OSRRKAYP.Entities;
+using Mapster;
+
+namespace OSRRKAYP.BusinessLogic.Mappings
+{
+    public class MappingRegister : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<Product, BrandResponse>()
+                .Map(br => br.BrandName, p => p.Name);
+
+            config.NewConfig<User, User>()
+                .Map(ud => ud.Role, u => u.Role);
+        }
+    }
+}
