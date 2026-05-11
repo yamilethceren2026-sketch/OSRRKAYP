32112a174374c39f7d61ce42f8f87ee83ed4cfaf
Oscar Andres Ramirez Romero <barleyrom0@gmail.com>
2026-05-10 21:46:01 -0600

    Feat: agregar carpeta GetProduct y la clases GetProductQuerie.cs y GetProductHandler  Realizado por : Oscar Andres Ramirez Romero

---
+++ OSRRKAYP.BusinessLogic/UseCases/Products/Queries/GetProduct/GetProductHandler.cs
@@ -0,0 +1,23 @@
+﻿using OSRRKAYP.BusinessLogic.DTOs;
+using OSRRKAYP.DataAccess.Interfaces;
+using OSRRKAYP.Entities;
+using Mapster;
+using MediatR;
+
+namespace OSRRKAYP.BusinessLogic.UseCases.Products.Queries.GetProduct
+{
+    internal sealed class GetProductHandler(IEfRepository<Product> _repository) : IRequestHandler<GetProductQuery, BrandResponse>
+    {
+        public async Task<BrandResponse> Handle(GetProductQuery query, CancellationToken cancellationToken)
+        {
+            var product = await _repository.GetByIdAsync(query.ProductId, cancellationToken);
+
+            if (product is null)
+            {
+                return new BrandResponse();
+            }
+
+            return product.Adapt<BrandResponse>();
+        }
+    }
+}
---
+++ OSRRKAYP.BusinessLogic/UseCases/Products/Queries/GetProduct/GetProductQuery.cs
@@ -0,0 +1,6 @@
+﻿using OSRRKAYP.BusinessLogic.DTOs;
+using MediatR;
+
+namespace OSRRKAYP.BusinessLogic.UseCases.Products.Queries.GetProduct;
+
+public record GetProductQuery(long ProductId) : IRequest<BrandResponse>;
