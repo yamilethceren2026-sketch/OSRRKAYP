54e0944e0e035a0224427bf61f2238c8e1801fb9
MINEDUCYT <MINEDUCYT@YAMICOMPU>
2026-05-12 06:05:43 -0600

    Feat : agregar Controlador BrandController.cs Realizado por: Oscar Andres Ramirez Romero

---
+++ OSRRKAYP.WebApplication/Controllers/BrandController.cs
@@ -0,0 +1,138 @@
+﻿using Mapster;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OSRRKAYP.BusinessLogic.UseCases.Brands.Queries.GetBrand;
+
+
+namespace OSRRKAYP.WebApplication.Controllers
+{
+    [Authorize]
+    public class BrandController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public BrandController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var brands = await _mediator.Send(new GetBrandsQuery());
+
+            return View(brands);
+        }
+
+        // GET: BrandController/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BrandController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create
+            (CreateBrandRequest createBrandRequest)
+        {
+            try
+            {
+                var result = await _mediator
+                    .Send(new CreateBrandCommand(createBrandRequest));
+
+                if (result > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    throw new Exception
+                        ("Sucedió un error al guardar la marca");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+
+                return View(createBrandRequest);
+            }
+        }
+
+        // GET: BrandController/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            var brand = await _mediator
+                .Send(new GetBrandQuery(id));
+
+            return View(brand.Adapt(new UpdateBrandRequest()));
+        }
+
+        // POST: BrandController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit
+            (UpdateBrandRequest updateBrandRequest)
+        {
+            try
+            {
+                var result = await _mediator
+                    .Send(new UpdateBrandCommand(updateBrandRequest));
+
+                if (result > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    throw new Exception
+                        ("Sucedió un error al editar la marca");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+
+                return View(updateBrandRequest);
+            }
+        }
+
+        // GET: BrandController/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            var brand = await _mediator
+                .Send(new GetBrandQuery(id));
+
+            return View(brand);
+        }
+
+        // POST: BrandController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete
+            (int id, BrandDto brandDto)
+        {
+            try
+            {
+                var result = await _mediator
+                    .Send(new DeleteBrandCommand(id));
+
+                if (result > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    throw new Exception
+                        ("Sucedió un error al eliminar la marca");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+
+                return View(brandDto);
+            }
+        }
+    }
+}
