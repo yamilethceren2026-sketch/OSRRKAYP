cb2fbd1467b82fbb122b9434ffee1c17d3879c18
MINEDUCYT <MINEDUCYT@YAMICOMPU>
2026-05-12 08:33:11 -0600

    Feat : crear carpeta Quotation y sus vistas Realizado por: Karla Yamileth Ceren Perez

---
+++ OSRRKAYP.BusinessLogic/DTOs/QuotationDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OSRRKAYP.BusinessLogic.DTOs
+{
+    public class QuotationDto
+    {
+        public long QuotationId { get; set; }
+
+        public string? ClientName { get; set; }
+
+        public string? ClientPhone { get; set; }
+
+        public string? SellerName { get; set; }
+
+        public string? PaymentMethodName { get; set; }
+
+        public long? QuotationNumber { get; set; }
+
+        public int? ValidityDays { get; set; }
+
+        public DateTime? QuotationRegistration { get; set; }
+
+        public bool QuotationStatus { get; set; }
+
+        public decimal? Total { get; set; }
+    }
+}
